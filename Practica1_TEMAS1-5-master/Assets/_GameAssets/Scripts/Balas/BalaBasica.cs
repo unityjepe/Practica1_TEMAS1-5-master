711386fe3a68b9f32a2a60c4354592c361a10cfd
Jepe <unityjepe@gmail.com>
2018-10-09 13:45:51 +0200

    GeneratorEnemigos

---
+++ Practica1_TEMAS1-5-master/Assets/_GameAssets/Scripts/Balas/BalaBasica.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BalaBasica : MonoBehaviour
+{
+    [SerializeField] GameObject prefobjBala;
+    //public Button yourButton; 
+
+    void Start()
+    {
+        //Button btn = yourButton.GetComponent<Button>();
+        // btn.onClick.AddListener();
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Disparo");
+            GameObject NuevaBala = Instantiate(prefobjBala, transform.position, transform.rotation);
+            NuevaBala.GetComponent<Rigidbody>().AddForce(Vector3.forward * 100);
+
+        }
+    }
+}
